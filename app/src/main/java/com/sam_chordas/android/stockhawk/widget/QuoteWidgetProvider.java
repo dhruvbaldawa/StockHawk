1effe5c784c13131076204064c493d786a348809
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2016-04-03 22:53:50 +0530

    add static widget

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/QuoteWidgetProvider.java
@@ -0,0 +1,32 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.ui.MyStocksActivity;
+
+/**
+ * Created by dhruv on 4/3/16.
+ */
+public class QuoteWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_large);
+
+            views.setTextViewText(R.id.stock_symbol, "FOOO");
+            views.setTextViewText(R.id.bid_price, "2");
+            views.setTextViewText(R.id.change, "10");
+
+            Intent launchIntent = new Intent(context, MyStocksActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, 0);
+            views.setOnClickPendingIntent(R.id.stock_symbol, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
